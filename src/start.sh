372fd46cb22933fefe60f9f2294ac0a4b115b3e9
boreddaoist <darlsmtl@gmail.com>
2025-04-21 18:26:51 +0200

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -4,6 +4,22 @@
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
+
+# Check if /runpod-volume/ComfyUI/models/insightface is mounted
+if [ -d "/runpod-volume/ComfyUI/models/insightface" ]; then
+    echo "/runpod-volume/ComfyUI/models/insightface directory exists."
+
+    # Create a soft link to /comfyui/models/insightface if it doesn't already exist
+    if [ ! -L "/comfyui/models/insightface" ]; then
+        ln -s /runpod-volume/ComfyUI/models/insightface /comfyui/models/insightface
+        echo "Created a soft link to /comfyui/models/insightface."
+    else
+        echo "Soft link already exists."
+    fi
+else
+    echo "/runpod-volume/ComfyUI/models/insightface directory does not exist."
+fi
+
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
     echo "runpod-worker-comfy: Starting ComfyUI"
